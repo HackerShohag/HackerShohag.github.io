311c8d0daed0c705bf1914e0617200c51d842226
HackerShohag <HackerShohag@outlook.com>
2023-09-05 22:34:50 +0600

    radius on tilt glare

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,38 +1,37 @@
-const imageWrapper = document.querySelector('.slideshow-wrapper')
-const imageItems = document.querySelectorAll('.slideshow-wrapper > *')
-const testimonialsItems = document.querySelectorAll('.testimonials-item')
-const imageLength = imageItems.length
-const perView = 2
-let totalScroll = 0
-const delay = 3000
+function runSlideShow(wrapperItem, hoverItem, itemsPerView, delay) {
+    const imageWrapper = document.querySelector(wrapperItem)
+    const imageItems = document.querySelectorAll(wrapperItem + ' > *')
+    const imageLength = imageItems.length
+    let totalScroll = 0
 
-function isHovered(className) {
-    return $("." + className + ":hover").length > 0;
-}
-
-imageWrapper.style.setProperty('--per-view', perView)
-for (let i = 0; i < perView; i++) {
-    imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
-}
-
-let autoScroll = setInterval(scrolling, delay)
-
-$(".testimonials-item").hover(function () {
-    clearInterval(autoScroll)
-}, function () {
-    autoScroll = setInterval(scrolling, delay)
-});
-
-function scrolling() {
-    totalScroll++
-    if (totalScroll == imageLength + 1) {
-        clearInterval(autoScroll)
-        totalScroll = 1
-        imageWrapper.style.transition = '0s'
-        imageWrapper.style.left = '0'
-        autoScroll = setInterval(scrolling, delay)
+    function isHovered(className) {
+        return $("." + className + ":hover").length > 0;
     }
-    const widthEl = document.querySelector('.slideshow-wrapper > :first-child').offsetWidth + 24
-    imageWrapper.style.left = `-${totalScroll * widthEl}px`
-    imageWrapper.style.transition = '.5s'
+
+    imageWrapper.style.setProperty('--per-view', itemsPerView)
+    for (let i = 0; i < itemsPerView; i++) {
+        imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+    }
+
+    let autoScroll = setInterval(scrolling, delay)
+
+    $(hoverItem).hover(function () {
+        clearInterval(autoScroll)
+    }, function () {
+        autoScroll = setInterval(scrolling, delay)
+    });
+
+    function scrolling() {
+        totalScroll++
+        if (totalScroll == imageLength + 1) {
+            clearInterval(autoScroll)
+            totalScroll = 1
+            imageWrapper.style.transition = '0s'
+            imageWrapper.style.left = '0'
+            autoScroll = setInterval(scrolling, delay)
+        }
+        const widthEl = document.querySelector(wrapperItem + ' > :first-child').offsetWidth + 24
+        imageWrapper.style.left = `-${totalScroll * widthEl}px`
+        imageWrapper.style.transition = '.5s'
+    }
 }
