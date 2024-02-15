16a58e21e7479a312e26f52306f9e34483be0528
HackerShohag <HackerShohag@outlook.com>
2024-02-16 03:56:41 +0600

    add google meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <meta name="google-site-verification" content="LfVzFPEr0IALyIIOvDAGXZB-jOcNdRrmHd3QdH5hYLE" />
+      </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
